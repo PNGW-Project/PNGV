c0ad89ad56ffed9a4062a1dc024bbd91fd453441
Nikolay Tunyan <nikolay.tunyan@guestmg.pro>
2026-02-16 11:26:21 +0400

    [PNGW- 8] First procedual generator code
    Added chunk creation code

---
+++ Source/First/Private/MapGenerator/AProceduralChunk.cpp
@@ -0,0 +1,74 @@
+﻿#include "MapGenerator/AProceduralChunk.h"
+
+/*
+Отвечает за:
+*Seed
+*Размер чанка
+*Радиус генерации
+*Отслеживание позиции камеры
+*Спавн / удаление чанков
+*/
+
+AProceduralChunk::AProceduralChunk()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    Mesh = CreateDefaultSubobject<UProceduralMeshComponent>(TEXT("Mesh"));
+    RootComponent = Mesh;
+}
+
+AProceduralChunk::~AProceduralChunk()
+{
+}
+
+float AProceduralChunk::GetHeight(float WorldX, float WorldY, int32 Seed) const
+{
+    float Scale = 0.001f;
+    return FMath::PerlinNoise2D(FVector2D(WorldX + Seed, WorldY + Seed) * Scale) * HeightMultiplier;
+}
+
+void AProceduralChunk::GenerateChunk(int32 ChunkX, int32 ChunkY, int32 Seed)
+{
+    TArray<FVector> Vertices;
+    TArray<int32> Triangles;
+    TArray<FVector> Normals;
+    TArray<FVector2D> UVs;
+    TArray<FProcMeshTangent> Tangents;
+
+    float Step = (float)ChunkSize / (VerticesPerLine - 1);
+
+    for (int32 y = 0; y < VerticesPerLine; y++)
+    {
+        for (int32 x = 0; x < VerticesPerLine; x++)
+        {
+            float WorldX = ChunkX * ChunkSize + x * Step;
+            float WorldY = ChunkY * ChunkSize + y * Step;
+
+            float Z = GetHeight(WorldX, WorldY, Seed);
+
+            Vertices.Add(FVector(x * Step, y * Step, Z));
+            UVs.Add(FVector2D((float)x / VerticesPerLine, (float)y / VerticesPerLine));
+            Normals.Add(FVector::UpVector);
+            Tangents.Add(FProcMeshTangent(1, 0, 0));
+        }
+    }
+
+    for (int32 y = 0; y < VerticesPerLine - 1; y++)
+    {
+        for (int32 x = 0; x < VerticesPerLine - 1; x++)
+        {
+            int32 i = x + y * VerticesPerLine;
+
+            Triangles.Add(i);
+            Triangles.Add(i + VerticesPerLine);
+            Triangles.Add(i + 1);
+
+            Triangles.Add(i + 1);
+            Triangles.Add(i + VerticesPerLine);
+            Triangles.Add(i + VerticesPerLine + 1);
+        }
+    }
+
+    Mesh->CreateMeshSection(0, Vertices, Triangles, Normals, UVs, {}, Tangents, true);
+}
+
---
+++ Source/First/Public/MapGenerator/AProceduralChunk.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProceduralMeshComponent.h"
+#include "ProceduralChunk.generated.h"
+
+/*
+ Отвечает за:
+*Генерацию terrain mesh
+*Построение вершин через noise
+*Создание ProceduralMesh 
+*/
+class FIRST_API AProceduralChunk : public AActor
+{
+public:
+	AProceduralChunk();
+	~AProceduralChunk();
+	void GenerateChunk(int32 ChunkX, int32 ChunkY, int32 Seed);
+
+protected:
+    UPROPERTY(VisibleAnywhere)
+    UProceduralMeshComponent* Mesh;
+
+private:
+    int32 ChunkSize = 2000;
+    int32 VerticesPerLine = 100;
+    float HeightMultiplier = 300.f;
+
+    float GetHeight(float WorldX, float WorldY, int32 Seed) const;
+};
